cad11d4fa9f1018e8b6e67925e6a23c949291e71
Shabbir Shakir <S.M.B75253@gmail.com>
2025-06-02 19:12:53 +0000

    studio-6128039683:~/studio{master}$ npx cap add android ✔ Adding native android project in android in 109.86ms ✔ add in 110.99ms ✖ copy android - failed! [error] The web assets directory (./public) must contain an index.html file.         It will be the entry point for the web portion of the Capacitor app. ✔ Updating Android plugins in 17.90ms ✖ update android - failed! [error] Error: ENOENT: no such file or directory, open         '/home/user/studio/android/app/src/main/assets/capacitor.plugins.json' studio-6128039683:~/studio{master}$ ^C

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.asharamubaraka.app',
+  appName: 'Asharamubaraka',
+  webDir: 'out' // Changed from 'public' to 'out'
+};
+
+export default config;
