8df98a69e76c9512d8f7cb2731b5a410bf24c301
Shabbir Shakir <S.M.B75253@gmail.com>
2025-06-03 05:25:58 +0000

    example manifest code {   "id": "/",   "dir": "ltr",   "lang": "en",   "name": "Webboard",   "scope": "/",   "display": "standalone",   "start_url": "/",   "short_name": "Webboard",   "theme_color": "#FFFFFF",   "description": "Enhance your work day and solve your cross platform whiteboarding needs with webboard! Draw text, shapes, attach images and more and share those whiteboards with anyone through OneDrive!",   "orientation": "any",   "background_color": "#FFFFFF",   "prefer_related_applications": false,   "display_override": [     "window-controls-overlay"   ],   "screenshots": [     {       "src": "assets/screen.png"     },     {       "src": "assets/screen.png"     },     {       "src": "assets/screen.png"     }   ],   "features": [     "Cross Platform",     "low-latency inking",     "fast",     "useful AI"   ],   "icons": [     {       "src": "/icons/manifest-icon-192.maskable.png",       "sizes": "192x192",       "type": "image/png",       "purpose": "any"     },     {       "src": "/icons/manifest-icon-192.maskable.png",       "sizes": "192x192",       "type": "image/png",       "purpose": "maskable"     },     {       "src": "/icons/manifest-icon-512.maskable.png",       "sizes": "512x512",       "type": "image/png",       "purpose": "any"     },     {       "src": "/icons/manifest-icon-512.maskable.png",       "sizes": "512x512",       "type": "image/png",       "purpose": "maskable"     }   ],   "categories": [     "productivity"   ],   "launch_handler": {     "client_mode": "navigate-existing"   },   "edge_side_panel": {},   "shortcuts": [     {       "name": "Start Live Session",       "short_name": "Start Live",       "description": "Jump direction into starting or joining a live session",       "url": "/?startLive",       "icons": [         {           "src": "/assets/icon/256.png",           "sizes": "256x256"         }       ]     }   ] }

--- android/app/src/main/java/com/asharamubaraka/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.asharamubaraka.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.asharamubaraka.app',
-  appName: 'Asharamubaraka',
-  webDir: 'out' // Changed from 'public' to 'out'
-};
-
-export default config;
